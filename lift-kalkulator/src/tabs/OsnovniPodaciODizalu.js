b383726913c6c7dccf6194f78d1cf18abddb1806
Enzo Grubisa <enzo.grubisa@gmail.com>
2022-11-09 19:08:33 +0100

    9.11.2022.

--- lift-kalkulator/src/tabs/OsnovniPodaciODizalu.js
+++ lift-kalkulator/src/tabs/OsnovniPodaciODizalu.js
@@ -41,7 +41,7 @@ const OsnovniPodaciODizalu = () => {
     };
 
     const bezStrojarniceClick = (e) => {
-        setBezStrojarnice(e.target.value);
+        setBezStrojarnice(e.target.checked);
     };
     
 
@@ -70,109 +70,105 @@ const OsnovniPodaciODizalu = () => {
         console.log("ukrcavanjeVilicaremBool = " + ukrcavanjeVilicaremBool);
         console.log("vrstaDizala = " + vrstaDizala);
         console.log("vrstaPogona = " + vrstaPogona);
+        console.log("bezStrojarnice = " + bezStrojarnice);
         console.log("-------");
     }
 
     function izracuniButtonClick(){
-        console.log("izracune treba napravit!");
+        alert("izracune treba napravit!");
     }
 
     return (
-        <div className='container-sm'>
-            <h2>Osnovni podaci o dizalu</h2>
-            
-            <div>
-                <label>Namjena dizala: </label>
-            </div>
-            <div>
-                
-                <select onChange={changeNamjenaDizala} defaultValue={namjenaDizala} className="form-select" aria-label="Default select example">
-                    <option value="osobno">Osobno dizalo</option>
-                    <option value="teretno">Teretno dizalo</option>
-                    <option value="osobnoTeretno">Osobno teretno dizalo</option>
-                </select>
-                <br/>
-                {/* { showUkrcavanjeVilicaremCheckBox ? <UkrcavanjeVilicarem ukrcavanjeVilicaremBool={ukrcavanjeVilicaremBool}/> : null } */}
-                { showUkrcavanjeVilicaremCheckBox ? 
-                <div>
-                    <input onClick={ukrcavanjeVilicaremCheckBoxClick} className="form-check-input" type="checkbox" value="" id="ukrcavanjeVilicaremCheckBox"/>
-                    <label className="form-check-label" htmlFor="ukrcavanjeVilicaremCheckBox">ukrcavanje viličarem</label>
-                </div> 
-                : null 
-                }
-            </div>
-            <br/>
-
-            <div>
-                <label>Vrsta dizala: </label>
-            </div>
-            <div>
-                <select onChange={changeVrstaDizala} defaultValue={vrstaDizala} className="form-select" aria-label="Default select example">
-                    <option value="elektricno">Električno dizalo s pogonskom užnicom</option>
-                    <option value="hidraulicno">Hidraulično dizalo</option>
-                </select>
-            </div>
-
-            <div>
-                <label>Vrsta pogona: </label>
-            </div>
-            <div>
-                <select value={vrstaPogona} onChange={changeVrstaPogona} className="form-select" aria-label="Default select example">
-                    {vrstaDizala === "elektricno" ? <option value="bezreduktorski">Bezreduktorski</option> : null}
-                    {vrstaDizala === "elektricno" ? <option value="reduktorski">Reduktorski</option> : null}
-                    {vrstaDizala === "hidraulicno" ? <option value="indirektni">Indirektni</option> : null}
-                    {vrstaDizala === "hidraulicno" ? <option value="direktni">Direktni</option> : null}
-                </select>
-            </div>
-            <br/>
-
-            <div>
-                <label>Smještaj pogona: </label>
-            </div>
-            <div>
-                <select value={smjestajPogona} onChange={changeSmjestajPogona} className="form-select" aria-label="Default select example">
-                    {vrstaDizala === "elektricno" && bezStrojarnice === true ?
-                    <>
-                        <option value="uVoznomOknuNaNosacu">U voznom oknu - na nosaču</option>
-                        <option value="uVoznomOknuNaVodilicama">U voznom oknu - na vodilicama</option>
-                        <option value="naDnuVoznogOkna">Na dnu voznog okna</option>
-                    </>
-                    : null}
-
-                    {vrstaDizala === "hidraulicno" && bezStrojarnice === true ?
-                    <>
-                        <option value="uVoznomOknu">U voznom oknu</option>
-                    </>
-                    : null}
+        <div className='container-fluid' style={{backgroundColor: "#CCCCCC"}}>
+            <div className="row">
+                <div className="col-sm">
+                    <h2>Osnovni podaci o dizalu</h2>
                     
-                    {bezStrojarnice === false ?
-                    <>
-                        <option value="uStrojarniciIznadVoznogOkna">U strojarnici iznad voznog okna</option>
-                        <option value="uStrojarniciIspodVoznogOkna">U strojarnici ispod voznog okna</option>
-                        <option value="uStrojarniciPoredVoznogOkna">U strojarnici pored voznog okna</option>
-                    </>
-                    : null}
-                </select>
-                <br/>
-                <input onClick={bezStrojarniceClick} className="form-check-input" type="checkbox" value="" id="bezStrojarniceCheckBox"/>
-                <label className="form-check-label" htmlFor="bezStrojarniceCheckBox">bez strojarnice</label>
+                    {/* namjena dizala */}
+                    <div className='divDropDown'>
+                        <label className='autoWidth'>Namjena dizala: </label>
+                        <select  onChange={changeNamjenaDizala} defaultValue={namjenaDizala} className="form-select" aria-label="Default select example">
+                            <option value="osobno">Osobno dizalo</option>
+                            <option value="teretno">Teretno dizalo</option>
+                            <option value="osobnoTeretno">Osobno teretno dizalo</option>
+                        </select>
+                        {/* { showUkrcavanjeVilicaremCheckBox ? <UkrcavanjeVilicarem ukrcavanjeVilicaremBool={ukrcavanjeVilicaremBool}/> : null } */}
+                        { showUkrcavanjeVilicaremCheckBox ? 
+                        <div className='divCheckBox'>
+                            <input className="form-check-input chkbx" onClick={ukrcavanjeVilicaremCheckBoxClick} type="checkbox" value="" id="ukrcavanjeVilicaremCheckBox"/>
+                            <label className="form-check-label" htmlFor="ukrcavanjeVilicaremCheckBox">ukrcavanje viličarem</label>
+                        </div> 
+                        : null 
+                        }
+                    </div>
+
+                    <br/><br/>
+
+                    <div className='divDropDown'>
+                        <label>Vrsta dizala: </label>
+                        <select onChange={changeVrstaDizala} defaultValue={vrstaDizala} className="form-select" aria-label="Default select example">
+                            <option value="elektricno">Električno dizalo s pogonskom užnicom</option>
+                            <option value="hidraulicno">Hidraulično dizalo</option>
+                        </select>
+                    </div>
+
+                    <div className='divDropDown'>
+                        <label>Vrsta pogona: </label>
+                        <select value={vrstaPogona} onChange={changeVrstaPogona} className="form-select" aria-label="Default select example">
+                            {vrstaDizala === "elektricno" ? <option value="bezreduktorski">Bezreduktorski</option> : null}
+                            {vrstaDizala === "elektricno" ? <option value="reduktorski">Reduktorski</option> : null}
+                            {vrstaDizala === "hidraulicno" ? <option value="indirektni">Indirektni</option> : null}
+                            {vrstaDizala === "hidraulicno" ? <option value="direktni">Direktni</option> : null}
+                        </select>
+                    </div>
+                    <br/>
+
+                    <div className='divDropDown'>
+                        <label>Smještaj pogona: </label>
+                        <select value={smjestajPogona} onChange={changeSmjestajPogona} className="form-select" aria-label="Default select example">
+                            {vrstaDizala === "elektricno" && bezStrojarnice === true ?
+                            <>
+                                <option value="uVoznomOknuNaNosacu">U voznom oknu - na nosaču</option>
+                                <option value="uVoznomOknuNaVodilicama">U voznom oknu - na vodilicama</option>
+                                <option value="naDnuVoznogOkna">Na dnu voznog okna</option>
+                            </>
+                            : null}
+
+                            {vrstaDizala === "hidraulicno" && bezStrojarnice === true ?
+                            <>
+                                <option value="uVoznomOknu">U voznom oknu</option>
+                            </>
+                            : null}
+                            
+                            {bezStrojarnice === false ?
+                            <>
+                                <option value="uStrojarniciIznadVoznogOkna">U strojarnici iznad voznog okna</option>
+                                <option value="uStrojarniciIspodVoznogOkna">U strojarnici ispod voznog okna</option>
+                                <option value="uStrojarniciPoredVoznogOkna">U strojarnici pored voznog okna</option>
+                            </>
+                            : null}
+                        </select>
+                        <div className='divCheckBox'>
+                            <input value={bezStrojarnice} onClick={bezStrojarniceClick} className="form-check-input chkbx" type="checkbox"  id="bezStrojarniceCheckBox"/>
+                            <label className="form-check-label" htmlFor="bezStrojarniceCheckBox">bez strojarnice</label>
+                        </div>
+                    </div>
+                    <br/>
+
+                    <br/>
+                    <br/>
+
+                    <button onClick={logButtonClick}>LOG</button>
+                    
+                    <br/>
+                    <br/>
+
+                    <button onClick={izracuniButtonClick}>Izracuni</button>
+                    <br/>
+                </div>
+                <div className="col-sm"></div>
+                <div className="col-sm"></div>
             </div>
-            <br/>
-
-
-
-
-            <br/>
-            <br/>
-
-
-            <button onClick={logButtonClick}>LOG</button>
-            <br/>
-
-            <br/>
-
-            <button onClick={izracuniButtonClick}>Izracuni</button>
-            <br/>
         </div>
       
     );
