78b8742b75e796cd906c46dc7a3f761eba299310
Enzo Grubisa <enzo.grubisa@gmail.com>
2025-02-18 20:28:00 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     port: "3306",
-    password: "LoRaRulez!",
+    password: "",
     database: "liftkalkulator"
 });
 
